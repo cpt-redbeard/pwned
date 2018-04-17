5b7a134af8f01f55c597375ac6bd1b1f0b9e65c6
pthomson <patrick@Patricks-MacBook-Pro.local>
2018-04-17 19:35:46 -0400

    curl argument change
    argument change to cause less noise on the output

--- pwned.sh
+++ pwned.sh
@@ -6,7 +6,7 @@ do
 owned="200"
 pwned_verdict=$(curl -s -o /dev/null -w "%{http_code}" 'https://haveibeenpwned.com/api/v2/breachedaccount/'$pwned'?truncateResponse=true')
 sleep 2
-pwned_reason=$(curl -L -s -o /dev/null -w "%{http_code}"-A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41' 'https://haveibeenpwned.com/api/v2/breachedaccount/'$pwned'?truncateResponse=true')
+pwned_reason=$(curl -s -A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41' 'https://haveibeenpwned.com/api/v2/breachedaccount/'$pwned'?truncateResponse=true')
 echo $pwned_reason
 if [ $pwned_verdict == $owned ]
 then
