4b0533347e0f663ec7d82e43129990a4d8ff3cfe
pthomson <patrick@Patricks-MacBook-Pro.local>
2018-04-17 19:31:56 -0400

    add pwned sites
    adding additional curl command to display what site was the cause of pwnage

--- pwned.sh
+++ pwned.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 for pwned in $(cat emails.txt)
 do
-	echo ####
    echo "Processing $pwned..."
-   #echo ####
-   #setting owned to a successful response to compare later
+   echo ####
 owned="200"
-#quotes were incorrect in the curl command, it wasn't passing it the email, it was passing $pwned as the value. Very tricky
 pwned_verdict=$(curl -s -o /dev/null -w "%{http_code}" 'https://haveibeenpwned.com/api/v2/breachedaccount/'$pwned'?truncateResponse=true')
-
-#used to debug what the status was after the curl
-#echo $pwned_verdict
-
+sleep 2
+pwned_reason=$(curl -L -s -o /dev/null -w "%{http_code}"-A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41' 'https://haveibeenpwned.com/api/v2/breachedaccount/'$pwned'?truncateResponse=true')
+echo $pwned_reason
 if [ $pwned_verdict == $owned ]
 then
 echo $pwned "has been pwned."
