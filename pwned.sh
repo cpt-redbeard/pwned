5d661413fcbed11361eadcb15ed882157fe25523
pthomson <patrick@Patricks-MacBook-Pro.local>
2018-08-13 12:58:37 -0400

    Oopsies on the EOF
    Had to fix the EOF and forgot to update source with the '!' for the if statement.

--- pwned.sh
+++ pwned.sh
@@ -2,7 +2,7 @@
 clear
 
 
-cat << eof
+cat << 'EOF'
  _   _                   _____       
 | | | |                 |_   _|      
 | |_| | __ ___   _____    | |        
@@ -23,7 +23,7 @@ cat << eof
 | .__/ \_/\_/ |_| |_|\___|\__,_|(_)  
 | |                                  
 |_|                                  
-eof
+EOF
 
 
 input="emails.txt"
@@ -42,7 +42,7 @@ pwned_verdict=$(curl -s -o /dev/null -w "%{http_code}" "https://haveibeenpwned.c
 sleep 2
 pwned_reason=$(curl -s -A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41' "https://haveibeenpwned.com/api/v2/breachedaccount/""$pwned""?truncateResponse=true")
 
-if [ -z "$pwned_reason" ]
+if [ ! -z "$pwned_reason" ]
 then
 echo "***""$pwned_reason""***"
 fi
