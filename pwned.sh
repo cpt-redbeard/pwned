90b7775d3d665ee9a4c8bac53a5fd97f19eada07
pthomson <patrick@Patricks-MacBook-Pro.local>
2018-08-13 12:31:22 -0400

    beautify

--- pwned.sh
+++ pwned.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-for pwned in $(cat emails.txt)
+
+input="/path/to/txt/file"
+while IFS= read -r pwned
 do
+#output for each email address
+   echo ####
    echo "Processing $pwned..."
    echo ####
+#html response code for pwned
 owned="200"
+
+#curl to get status
 pwned_verdict=$(curl -s -o /dev/null -w "%{http_code}" "https://haveibeenpwned.com/api/v2/breachedaccount/""$pwned""?truncateResponse=true")
+   #sleep is meant to not overload the API
 sleep 2
 pwned_reason=$(curl -s -A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41' "https://haveibeenpwned.com/api/v2/breachedaccount/""$pwned""?truncateResponse=true")
 echo "$pwned_reason"
+
+
 if [ "$pwned_verdict" == "$owned" ]
 then
 echo "$pwned" "has been pwned."
@@ -16,7 +26,8 @@ echo "$pwned" "is good to go."
 fi
 
    echo ####
-   echo ####
+   #echo ####
+   #sleep is meant to not overload the API
    sleep 2
 
-done
+done < "$input"
